2866ff4d27507e05b751ed3c12b3dd594c3316a6
Rolf Bansbach <rolfisub@gmail.com>
2018-06-16 23:25:31 -0400

    settig up config form 2

--- src/forms/config.form.tsx
+++ src/forms/config.form.tsx
@@ -1,28 +1,81 @@
 import * as React from 'react';
 import { renderTextField } from '../common/render.fields';
-import {InjectedFormProps} from "redux-form";
-import {ConfigElevator} from "./config.types";
+import {Field, InjectedFormProps, default as reduxForm} from 'redux-form';
+import { ConfigElevator } from './config.types';
+import { Button } from '@material-ui/core';
 
-interface ConfigFormProps extends InjectedFormProps<ConfigElevator> {
+interface ConfigFormProps {
     handleSubmit: () => void;
 }
 
-class ConfigForm extends React.Component<ConfigFormProps> {
-
-    constructor(props:ConfigFormProps) {
+class ConfigForm extends React.Component<
+    ConfigFormProps & InjectedFormProps<ConfigElevator>
+> {
+    constructor(props: any) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit() {
+    handleSubmit(config: ConfigElevator) {
         return;
     }
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.handleSubmit)} >
-
+            <form onSubmit={this.props.handleSubmit(this.handleSubmit)}>
+                <div>
+                    <Field
+                        name={'floors'}
+                        label={'Floors'}
+                        component={renderTextField}
+                    />
+                    <Field
+                        name={'elevators'}
+                        label={'Number of Elevators'}
+                        component={renderTextField}
+                    />
+                    <Field
+                        name={'elevatorCapacity'}
+                        label={'Elevator Capacity'}
+                        component={renderTextField}
+                    />
+                    <Button
+                        variant={'raised'}
+                        color={'primary'}
+                        style={{ margin: 10 }}
+                        disabled={this.props.invalid}
+                        type={'submit'}
+                    >
+                        Generate
+                    </Button>
+                </div>
             </form>
         );
     }
 }
+
+export const ConfigFormReduxForm = reduxForm( {
+    form: 'ConfigForm'
+})(ConfigForm);
+
+const mapStateToProps = (state: , props:ConfigFormProps): ConfigFormProps => {
+    const redux: BlueprintCreateProps = {
+        submitErrors: getFormSubmitErrors('BlueprintCreateForm')(state),
+        ...props
+    };
+
+    return redux;
+};
+
+const mapDispatchToProps = (
+    dispatch: Dispatch<ThunkAction<void, BlueprintStoreState, void> | any>,
+    props: BlueprintCreateProps
+) => {
+    const redux: BlueprintCreateProps = {
+        createBlueprint: (s: Blueprint) => {
+            dispatch(blueprintActionCreators.create(s));
+        },
+        ...props
+    };
+    return redux;
+};
