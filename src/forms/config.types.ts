9d0714602774b7df283dfae2aa4782e35694275e
Rolf Bansbach <rolfbans@gmail.com>
2018-06-16 23:25:31 -0400

    settig up config form 2

--- src/forms/config.types.ts
+++ src/forms/config.types.ts
@@ -1,7 +1,9 @@
-import { Model } from '../common/redux.common';
+import {Model, StoreState} from '../common/redux.common';
 
 export interface ConfigElevator extends Model {
     floors: number;
     elevators: number;
     elevatorCapacity: number;
 }
+
+export interface ConfigElevatorState extends StoreState<ConfigElevator> {}
