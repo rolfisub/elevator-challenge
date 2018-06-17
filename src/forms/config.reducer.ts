d6b52b452794173418bf491e9b9ddf4bbed0b77c
Rolf Bansbach <rolfbans@gmail.com>
2018-06-16 23:58:31 -0400

    wiring up reducer and more for config form

---
+++ src/forms/config.reducer.ts
@@ -0,0 +1,31 @@
+import {ConfigElevator, ConfigElevatorActionTypes, ConfigElevatorState} from "./config.types";
+import {Action} from "../common/redux.common";
+
+export default (
+    state: ConfigElevatorState = {
+        current: {
+            _id: 0,
+            elevators: 2,
+            floors: 5,
+            elevatorCapacity: 1
+        },
+        list: []
+    },
+    action: Action<ConfigElevator>
+) => {
+    switch (action.type) {
+        case ConfigElevatorActionTypes.create: {
+            if(action.payload && state.current) {
+                const newState: ConfigElevatorState = {
+                    current: {...action.payload.data},
+                    list: [...state.list]
+                };
+                return newState;
+            }
+            break;
+        }
+        default:
+            return state;
+    }
+    return state
+}
