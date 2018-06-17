f173031e0b06a59d14e0a65bd33c43e77cf64c48
Rolf Bansbach <rolfbans@gmail.com>
2018-06-16 22:55:01 -0400

    set up redux and other dependencies

---
+++ src/common/redux.common.ts
@@ -0,0 +1,19 @@
+export interface Model {
+    _id: number;
+}
+
+export interface StoreState<M extends Model> {
+    current: M;
+    list: M[];
+}
+
+export interface Payload<M extends Model> {
+    data: M;
+    list: M[];
+}
+
+export interface Action<M extends Model> {
+    type: string;
+    payload?: Payload<M>;
+}
+
